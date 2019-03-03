d5180790187bdf206a85f9c7f8378fdfa5ebf901
Matteo Crippa <matteo@boostco.de>
2019-03-03 20:34:09 +0100

    footer cleanup

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,8 +1,14 @@
 <template>
-    <v-footer class="pa-3">
-        <v-spacer></v-spacer>
-        <div>&copy; 2016 - {{ year }} BoostCode - <a href="https://github.com/boostcode" target="_blank">GitHub</a> - <a href="https://twitter.com/boostcode" target="_blank">Twitter</a> - <router-link :to="{ path: '/about-us' }" active-class="noneclass">About Us</router-link> - <a href="https://www.iubenda.com/privacy-policy/7904617" target="_blank">Privacy</a> - <a href="https://www.iubenda.com/privacy-policy/7904617/cookie-policy" target="_blank">Cookie</a> Policy</div>
-    </v-footer>
+  <v-footer class="pa-3">
+    <v-spacer></v-spacer>
+    <div>
+      &copy; 2016 - {{ year }} BoostCode -
+      <a href="https://github.com/boostcode" target="_blank">GitHub</a> -
+      <a href="https://twitter.com/boostcode" target="_blank">Twitter</a> -
+      <a href="https://www.iubenda.com/privacy-policy/7904617" target="_blank">Privacy</a> -
+      <a href="https://www.iubenda.com/privacy-policy/7904617/cookie-policy" target="_blank">Cookie</a> Policy
+    </div>
+  </v-footer>
 </template>
 
 <script>
@@ -10,5 +16,5 @@ export default {
   data: () => ({
     year: new Date().getFullYear()
   })
-}
+};
 </script>
